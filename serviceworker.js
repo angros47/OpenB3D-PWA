86e3e1d4a060452b6f15babf412b063a77503fda
angros47 <32495525+angros47@users.noreply.github.com>
2023-01-07 23:06:16 +0100

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,18 +1,18 @@
 var cacheName = "Flightsim-pwa";
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/OpenB3D.wasm',
-  '/OpenB3D.js',
-  '/OpenB3DJS.js',
-  '/joy.js',
-  '/lmap_256.bmp',
-  '/hmap_1024.bmp',
-  '/FIGHTER.JPG',
-  '/fighter.3ds',
-  '/coolgrass2.bmp',
-  '/cloud_2.bmp',
-  '/water-2_mip.bmp'
+  './',
+  './index.html',
+  './OpenB3D.wasm',
+  './OpenB3D.js',
+  './OpenB3DJS.js',
+  './joy.js',
+  './lmap_256.bmp',
+  './hmap_1024.bmp',
+  './FIGHTER.JPG',
+  './fighter.3ds',
+  './coolgrass2.bmp',
+  './cloud_2.bmp',
+  './water-2_mip.bmp'
 ];
 
 /* Start the service worker and cache all of the app's content */
